fcc729902e99eb1afa9331b9b1c7e79443001cbf
Giovanna Gaffo <giovannagaffo21@gmail.com>
2026-09-08 22:13:00 -0300

    learn(ap03-react): lida com o codigo 2.7.2

---
+++ ap03-react-jsx/src/App.jsx
@@ -0,0 +1,15 @@
+//arrow function
+const App = () => {
+  return (
+    <div style={{margin: 'auto', width: 768, backgroundColor: '#EEE', 
+    padding: 12, borderRadius: 8}}>
+      <label
+        style={{display: 'block', marginBottom: 8}}
+        htmlFor="nome">
+          Nome:
+      </label>
+    </div>
+  )
+}
+
+export default App
