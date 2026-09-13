6332c0a4717adbad107d0414d633a1e4a061c055
Giovanna Gaffo <giovannagaffo21@gmail.com>
2026-09-13 20:31:25 -0300

    learn(ap06-react): encerra a secao 2.7
    -retomar fazendo o spinner
    -resolver o problema do timer

--- ap05-react-estado/src/App.jsx
+++ ap05-react-estado/src/App.jsx
@@ -1,55 +1,51 @@
 import React from "react"
+import EstacaoClimatica from "./EstacaoClimatica"
 
 class App extends React.Component {
 
-  constructor(props){
-    super(props)
-    this.state = {
-      latitude: null,
-      longitude: null,
-      estacao: null, 
-      data: null, 
-      icone: null,
-      mensagemDeErro: null
-    }
+  state = {
+    latitude: null,
+    longitude: null,
+    estacao: null,
+    icone: null,
+    mensagemDeErro: null
   }
 
-  render(){
+  componentDidMount() {
+    console.log('componenteDidMount')
+  }
+
+  componentDidUpdate() {
+    console.log('componenteDidUpdate')
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount')
+  }
+
+  render() {
+    console.log('render')
     return (
       <div className="container border rounded py-3 mt-2">
         <div className="row justify-content-center">
           <div className="col-12 col-md-8">
-            <div className="card">
-              <div className="card-body">
-                <div
-                  style={{height: '6rem'}}
-                  className="d-flex align-items-center border rounded mb-2">
-                    <i className={`fa-solid fa-${this.state.icone}`}></i>
-                    <p className="w-75 ms-3 text-center fs-1">
-                      {this.state.estacao}
-                    </p>
-                </div> 
-                <div>
-                  <p className="text-center">
-                    {
-                      this.state.latitude ? 
-                        `Coordenadas: ${this.state.latitude}, ${this.state.longitude}.
-                        Data: ${this.state.data}.` 
-                      :
-                        this.state.mensagemDeErro ? 
-                        `${this.state.mensagemDeErro}`
-                      :
-                        `Clique no botão para saber a sua estação climática`
-                    }
-                  </p>
-                </div>
-                <button
-                  onClick={this.obterLocalizacao}
-                  className="btn btn-outline-primary w-100 mt-2">
-                    Qual a minha estação? 
-                  </button>
-              </div>
-            </div>
+            {
+                this.state.mensagemDeErro ?
+                <p className='border rounded p-2 fs-1 text-center'>
+                  É preciso dar permissão para acesso à localização. Atualize a página e tente de novo, 
+                  ajustando a configuração no seu computador.
+                </p>
+                :
+                  <EstacaoClimatica
+                    icone={this.state.icone}
+                    estacao={this.state.estacao}
+                    latitude={this.state.latitude}
+                    longitude={this.state.longitude}
+                    mensagemDeErro={this.state.mensagemDeErro}
+                    obterLocalizacao={this.obterLocalizacao}
+                    root={this.props.root}
+                  />
+            }
           </div>
         </div>
       </div>
@@ -66,8 +62,8 @@ class App extends React.Component {
     const d3 = new Date(anoAtual, 11, 22)
     //21/03
     const d4 = new Date(anoAtual, 2, 21)
-    const estouNoSul = latitude < 0 
-    if(data >= d1 && data < d2)
+    const estouNoSul = latitude < 0
+    if (data >= d1 && data < d2)
       return estouNoSul ? 'Inverno' : 'Verão'
     if (data >= d2 && data < d3)
       return estouNoSul ? 'Primavera' : 'Outono'
@@ -77,26 +73,25 @@ class App extends React.Component {
   }
 
   icones = {
-    'Primavera' : 'seedling',
-    'Verão' : 'umbrella-beach',
-    'Outono' : 'tree',
-    'Inverno' : 'snowman'
+    'Primavera': 'seedling',
+    'Verão': 'umbrella-beach',
+    'Outono': 'tree',
+    'Inverno': 'snowman'
   }
 
   obterLocalizacao = () => {
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
-          const data = new Date()
-          const estacao = this.obterEstacao(data, position.coords.latitude)
-          const icone = this.icones[estacao]
-          //this.state.latitude = position.coords.latitude não dá pra fazer atribuição, porque a tela não atualiza, não estamos usando direito a variável de estado (não dispara a função render)
-          this.setState({
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-            estacao: estacao, 
-            data: data.toLocaleTimeString(),
-            icone: icone
-          })
+        const data = new Date()
+        const estacao = this.obterEstacao(data, position.coords.latitude)
+        const icone = this.icones[estacao]
+        //this.state.latitude = position.coords.latitude não dá pra fazer atribuição, porque a tela não atualiza, não estamos usando direito a variável de estado (não dispara a função render)
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          estacao: estacao,
+          icone: icone
+        })
       },
       (erro) => {
         console.log(`Erro: ${erro}`)
@@ -104,15 +99,15 @@ class App extends React.Component {
           mensagemDeErro: 'Tente novamente mais tarde'
         })
       }
-    ) 
-  
+    )
+
   }
 
 }
 
 export default App
 
-  
+
 
 /*variável de estado: quando atualizada, ela causa a atualização na tela. Associada a atualização na tela. 
 variável comum: quando atualizada, não causa atualiza na tela. 
--- ap05-react-estado/src/main.jsx
+++ ap05-react-estado/src/main.jsx
@@ -4,9 +4,10 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const root = createRoot(document.getElementById('root'))
+root.render(
+    <App root={root}/>
 )
 
+//permitir que o desenvolver encontre bugs em tempo de desenvolvimento, mas faz com que o componente seja construído 2x (StrictMode)
+//props: passa o objeto ao app, para que entregue dados a outro componente
